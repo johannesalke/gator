b516d5b416ceb478283e4374a727bae5bfe9ad68
johannesalke <johannesalke@gmail.com>
2026-03-17 00:50:29 +0100

    Can now read and set config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,65 @@
 package config
 
-import(
+import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const configFileName = ".gatorconfig.json"
 
-type Config struct{
-	DBUrl string `json:"db_url"`
+type Config struct {
+	DBUrl           string `json:"db_url"`
 	CurrentUsername string `json:"current_user_name"`
 }
 
+func getConfigFilePath() string {
+	homeDirPath, _ := os.UserHomeDir()
+	configFilePath := filepath.Join(homeDirPath, configFileName)
+	return configFilePath
+}
+
+func (cfg Config) write() error {
+	f, err := os.OpenFile(getConfigFilePath(), os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("Error while opening config file: %s", err)
+	}
+	defer f.Close()
+
+	fileEncoder := json.NewEncoder(f)
+	err = fileEncoder.Encode(cfg)
+	if err != nil {
+		return fmt.Errorf("Error while encoding config json: %s", err)
+	}
+	return nil
+}
 
 func Read() Config {
 
+	f, err := os.Open(getConfigFilePath())
+	if err != nil {
+		fmt.Printf("Error while reading config file: %s", err)
+		return Config{}
+	}
+	defer f.Close()
+
+	fDecoder := json.NewDecoder(f)
+	var cfg Config
+
+	err = fDecoder.Decode(&cfg)
+	if err != nil {
+		fmt.Printf("Error while reading file into config struct: %s", err)
+		return Config{}
+	}
+	return cfg
 }
 
-
-func (cfg Config) SetUser
+func (cfg Config) SetUser(new_user_name string) {
+	cfg.CurrentUsername = new_user_name
+	err := cfg.write()
+	if err != nil {
+		fmt.Printf("Error while reading file into config struct: %s", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"github.com/johannesalke/gator/internal/config"
+	"os"
+)
+
+func main() {
+	s, _ := os.UserHomeDir()
+	fmt.Printf("%s\n", s)
+	fmt.Printf("%s\n", config.Read())
+
+	var cfg config.Config
+	cfg = config.Read()
+	cfg.SetUser("johannes")
+	fmt.Printf("%s", cfg.CurrentUsername)
+
+	fmt.Print(config.Read())
+}
