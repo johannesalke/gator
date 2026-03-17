894c51da56b39d52569c6fee83549c541cef122e
johannesalke <johannesalke@gmail.com>
2026-03-17 02:46:43 +0100

    implemented login

---
+++ sql/schema/01_migration.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE users (
+    id  UUID primary key,
+    created_at TIMESTAMP not null,
+    updated_at TIMESTAMP not null,
+    name text not null
+
+);
+
+-- +goose Down
+DROP TABLE users;
+
